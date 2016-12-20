c274a1fafb148e0278db13dfeb57457367b8883c
Hylke Visser <htdvisser@gmail.com>
2016-12-20 15:56:40 +0100

    Add method for getting apex/log that logs to Stdout

--- log/apex/apex.go
+++ log/apex/apex.go
@@ -1,9 +1,20 @@
 package apex
 
 import (
+	"os"
+
+	"github.com/TheThingsNetwork/go-utils/handlers/cli"
 	"github.com/TheThingsNetwork/go-utils/log"
 	apex "github.com/apex/log"
 )
+
+// Stdout logging apex/log
+func Stdout() log.Interface {
+	return Wrap(&apex.Logger{
+		Level:   apex.InfoLevel,
+		Handler: cli.New(os.Stdout),
+	})
+}
 
 // Wrap apex.Interface
 func Wrap(ctx apex.Interface) log.Interface {
