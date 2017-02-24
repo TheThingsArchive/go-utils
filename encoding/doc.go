5569afcd2571d3da7b19296a7a2278dd3398f93e
Roman Volosatovs <rvolosatovs@riseup.net>
2017-02-24 15:33:41 +0100

    encoding: add documentation(wip)

---
+++ encoding/doc.go
@@ -0,0 +1,6 @@
+// Copyright © 2017 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+// Package encoding is responsible for encoding and decoding of structs.
+// `squash` option assumes that field names are unique in structs, otherwise results are unpredictable
+package encoding
