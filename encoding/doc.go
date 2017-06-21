255bc29d54510050cc9102f8b4c9e6d02ae1672e
Roman Volosatovs <rvolosatovs@riseup.net>
2017-06-21 03:52:19 +0200

    encoding: add `cast` doc

--- encoding/doc.go
+++ encoding/doc.go
@@ -3,4 +3,5 @@
 
 // Package encoding is responsible for encoding and decoding of structs.
 // `squash` option assumes that field names are unique in structs, otherwise results are unpredictable
+// `cast` option casts value to specified type on encoding to string-interface map
 package encoding
