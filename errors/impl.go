2d1fba6d45cb92ef0ba749de5310d65dd51f7dcf
Romeo Van Snick <romeovs@gmail.com>
2017-07-11 14:24:01 +0200

    Make imple json marshallable

--- errors/impl.go
+++ errors/impl.go
@@ -2,28 +2,42 @@ package errors
 
 // impl implements Error
 type impl struct {
-	message    string
-	code       Code
-	typ        Type
-	attributes Attributes
+	Imessage    string     `json:"error"`
+	Icode       Code       `json:"error_code,omitempty"`
+	Ityp        Type       `json:"error_type,omitempty"`
+	Iattributes Attributes `json:"attributes,omitempty"`
 }
 
 // Error returns the formatted error message
 func (i *impl) Error() string {
-	return i.message
+	return i.Imessage
 }
 
 // Code returns the error code
 func (i *impl) Code() Code {
-	return i.code
+	return i.Icode
 }
 
 // Type returns the error type
 func (i *impl) Type() Type {
-	return i.typ
+	return i.Ityp
 }
 
 // Attributes returns the error attributes
 func (i *impl) Attributes() Attributes {
-	return i.attributes
+	return i.Iattributes
+}
+
+// toImpl creates an equivalent impl for any Error
+func toImpl(err Error) *impl {
+	if i, ok := err.(*impl); ok {
+		return i
+	}
+
+	return &impl{
+		Imessage:    err.Error(),
+		Icode:       err.Code(),
+		Ityp:        err.Type(),
+		Iattributes: err.Attributes(),
+	}
 }
