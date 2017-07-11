c31e74768106e51860a1a516e858058da1a9a5c5
Romeo Van Snick <romeovs@gmail.com>
2017-07-11 14:23:44 +0200

    Add conversion from/to http response

--- errors/http.go
+++ errors/http.go
@@ -1,6 +1,12 @@
 package errors
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// CodeHeader is the header where the error code will be stored
+const CodeHeader = "X-TTN-Error-Code"
 
 // HTTPStatusCode returns the corresponding http status code from an error type
 func (t Type) HTTPStatusCode() int {
@@ -8,35 +14,25 @@ func (t Type) HTTPStatusCode() int {
 	case InvalidArgument:
 	case OutOfRange:
 		return http.StatusBadRequest
-
 	case NotFound:
 		return http.StatusNotFound
-
 	case Conflict:
 	case AlreadyExists:
 		return http.StatusConflict
-
 	case Unauthorized:
 		return http.StatusUnauthorized
-
 	case PermissionDenied:
 		return http.StatusForbidden
-
 	case Timeout:
 		return http.StatusRequestTimeout
-
 	case NotImplemented:
 		return http.StatusNotImplemented
-
 	case TemporarilyUnavailable:
 		return http.StatusBadGateway
-
 	case PermanentlyUnavailable:
 		return http.StatusGone
-
 	case ResourceExhausted:
 		return http.StatusTooManyRequests
-
 	case Unknown:
 	case Internal:
 	case Canceled:
@@ -55,4 +51,74 @@ func HTTPStatusCode(err error) int {
 	}
 
 	return http.StatusInternalServerError
+}
+
+// HTTPStatusToType infers the error Type from a HTTP Status code
+func HTTPStatusToType(status int) Type {
+	switch status {
+	case http.StatusBadRequest:
+		return InvalidArgument
+	case http.StatusNotFound:
+		return NotFound
+	case http.StatusConflict:
+		return Conflict
+	case http.StatusUnauthorized:
+		return Unauthorized
+	case http.StatusForbidden:
+		return PermissionDenied
+	case http.StatusRequestTimeout:
+		return Timeout
+	case http.StatusNotImplemented:
+		return NotImplemented
+	case http.StatusBadGateway:
+	case http.StatusServiceUnavailable:
+		return TemporarilyUnavailable
+	case http.StatusGone:
+		return PermanentlyUnavailable
+	case http.StatusTooManyRequests:
+		return ResourceExhausted
+	case http.StatusInternalServerError:
+		return Unknown
+	}
+	return Unknown
+}
+
+// FromHTTP parses the http.Response and returns the corresponding
+// If the response is not an error (eg. 200 OK), it returns nil
+func FromHTTP(resp *http.Response) Error {
+	if resp.StatusCode < 399 {
+		return nil
+	}
+
+	out := new(impl)
+
+	// try to decode the error
+	defer resp.Body.Close()
+	err := json.NewDecoder(resp.Body).Decode(out)
+	if err == nil {
+		return out
+	}
+
+	// fall back
+	out.Icode = parseCode(resp.Header.Get(CodeHeader))
+	out.Ityp = HTTPStatusToType(resp.StatusCode)
+	out.Imessage = out.Ityp.String()
+
+	return out
+}
+
+// ToHTTP writes the error to the http response
+func ToHTTP(in error, w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "application/json")
+	if err, ok := in.(Error); ok {
+		w.Header().Set(CodeHeader, err.Code().String())
+		w.WriteHeader(err.Type().HTTPStatusCode())
+		return json.NewEncoder(w).Encode(toImpl(err))
+	}
+
+	w.WriteHeader(http.StatusInternalServerError)
+	return json.NewEncoder(w).Encode(&impl{
+		Imessage: in.Error(),
+		Ityp:     Unknown,
+	})
 }
---
+++ errors/http_test.go
@@ -0,0 +1,58 @@
+package errors
+
+import (
+	"errors"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/smartystreets/assertions"
+)
+
+func TestHTTP(t *testing.T) {
+	a := assertions.New(t)
+
+	d := &ErrDescriptor{
+		MessageFormat: "You do not have access to app with id {app_id}",
+		Code:          77,
+		Type:          PermissionDenied,
+		registered:    true,
+	}
+
+	attributes := Attributes{
+		"app_id": "foo",
+		"count":  42,
+	}
+
+	err := d.New(attributes)
+
+	w := httptest.NewRecorder()
+	e := ToHTTP(err, w)
+	a.So(e, assertions.ShouldBeNil)
+
+	resp := w.Result()
+
+	got := FromHTTP(resp)
+	a.So(got.Code(), assertions.ShouldEqual, err.Code())
+	a.So(got.Type(), assertions.ShouldEqual, err.Type())
+	a.So(got.Error(), assertions.ShouldEqual, err.Error())
+	a.So(got.Attributes()["app_id"], assertions.ShouldResemble, attributes["app_id"])
+	a.So(got.Attributes()["count"], assertions.ShouldAlmostEqual, attributes["count"])
+}
+
+func TestToUnspecifiedHTTP(t *testing.T) {
+	a := assertions.New(t)
+
+	err := errors.New("A random error")
+
+	w := httptest.NewRecorder()
+	e := ToHTTP(err, w)
+	a.So(e, assertions.ShouldBeNil)
+
+	resp := w.Result()
+
+	got := FromHTTP(resp)
+	a.So(got.Code(), assertions.ShouldEqual, Code(0))
+	a.So(got.Type(), assertions.ShouldEqual, Unknown)
+	a.So(got.Error(), assertions.ShouldEqual, err.Error())
+	a.So(got.Attributes(), assertions.ShouldBeNil)
+}
