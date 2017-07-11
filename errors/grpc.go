f8b18ca2079bec23c944169ad90c04b6f5cb6d10
Romeo Van Snick <romeovs@gmail.com>
2017-07-11 14:23:21 +0200

    Add conversions from/to grpc errors

--- errors/grpc.go
+++ errors/grpc.go
@@ -1,14 +1,79 @@
 package errors
 
 import (
+	"encoding/json"
+	"regexp"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 )
 
 // GRPCCode returns the corresponding http status code from an error type
 func (t Type) GRPCCode() codes.Code {
-	// TODO
+	switch t {
+	case InvalidArgument:
+		return codes.InvalidArgument
+	case OutOfRange:
+		return codes.OutOfRange
+	case NotFound:
+		return codes.NotFound
+	case Conflict:
+	case AlreadyExists:
+		return codes.AlreadyExists
+	case Unauthorized:
+		return codes.Unauthenticated
+	case PermissionDenied:
+		return codes.PermissionDenied
+	case Timeout:
+		return codes.DeadlineExceeded
+	case NotImplemented:
+		return codes.Unimplemented
+	case TemporarilyUnavailable:
+		return codes.Unavailable
+	case PermanentlyUnavailable:
+		return codes.FailedPrecondition
+	case Canceled:
+		return codes.Canceled
+	case ResourceExhausted:
+		return codes.ResourceExhausted
+	case Internal:
+	case Unknown:
+		return codes.Unknown
+	}
+
 	return codes.Unknown
+}
+
+func GRPCCodeToType(code codes.Code) Type {
+	switch code {
+	case codes.InvalidArgument:
+		return InvalidArgument
+	case codes.OutOfRange:
+		return OutOfRange
+	case codes.NotFound:
+		return NotFound
+	case codes.AlreadyExists:
+		return AlreadyExists
+	case codes.Unauthenticated:
+		return Unauthorized
+	case codes.PermissionDenied:
+		return PermissionDenied
+	case codes.DeadlineExceeded:
+		return Timeout
+	case codes.Unimplemented:
+		return NotImplemented
+	case codes.Unavailable:
+		return TemporarilyUnavailable
+	case codes.FailedPrecondition:
+		return PermanentlyUnavailable
+	case codes.Canceled:
+		return Canceled
+	case codes.ResourceExhausted:
+		return ResourceExhausted
+	case codes.Unknown:
+		return Unknown
+	}
+	return Unknown
 }
 
 // GRPCCode returns the corresponding http status code from an error
@@ -19,4 +84,45 @@ func GRPCCode(err error) codes.Code {
 	}
 
 	return grpc.Code(err)
+}
+
+var regex = regexp.MustCompile(`.*desc = (.*) \(e:(\d+)\) attributes = (.*)`)
+var format = "%s (e:%v) attributes = %s"
+
+// FromGRPC parses a gRPC error and returns an Error
+func FromGRPC(in error) Error {
+	out := &impl{
+		Imessage: grpc.ErrorDesc(in),
+		Ityp:     GRPCCodeToType(grpc.Code(in)),
+		Icode:    Code(0),
+	}
+
+	matches := regex.FindStringSubmatch(in.Error())
+
+	if len(matches) < 4 {
+		return out
+	}
+
+	// set message
+	out.Imessage = matches[1]
+	out.Icode = parseCode(matches[2])
+	_ = json.Unmarshal([]byte(matches[3]), &out.Iattributes)
+
+	got := Get(Code(out.Icode))
+	if got == nil {
+		return out
+	}
+
+	// Todo: find attributes
+	return got.New(out.Iattributes)
+}
+
+// ToGRPC turns an error into a gRPC error
+func ToGRPC(in error) error {
+	if err, ok := in.(Error); ok {
+		attrs, _ := json.Marshal(err.Attributes())
+		return grpc.Errorf(err.Type().GRPCCode(), format, err.Error(), err.Code(), attrs)
+	}
+
+	return grpc.Errorf(codes.Unknown, in.Error())
 }
---
+++ errors/grpc_test.go
@@ -0,0 +1,59 @@
+package errors
+
+import (
+	"fmt"
+	"testing"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+
+	"github.com/smartystreets/assertions"
+)
+
+func TestGRPC(t *testing.T) {
+	a := assertions.New(t)
+	d := &ErrDescriptor{
+		MessageFormat: "You do not have access to app with id {app_id}",
+		Code:          77,
+		Type:          PermissionDenied,
+		registered:    true,
+	}
+
+	attributes := Attributes{
+		"app_id": "foo",
+		"count":  42,
+	}
+
+	err := d.New(attributes)
+
+	code := GRPCCode(err)
+	a.So(code, assertions.ShouldEqual, codes.PermissionDenied)
+
+	// other errors should be unknown
+	other := fmt.Errorf("Foo")
+	code = GRPCCode(other)
+	a.So(code, assertions.ShouldEqual, codes.Unknown)
+
+	grpcErr := ToGRPC(err)
+
+	got := FromGRPC(grpcErr)
+	a.So(got.Code(), assertions.ShouldEqual, d.Code)
+	a.So(got.Type(), assertions.ShouldEqual, d.Type)
+	a.So(got.Error(), assertions.ShouldEqual, "You do not have access to app with id foo")
+
+	a.So(got.Attributes(), assertions.ShouldNotBeEmpty)
+	a.So(got.Attributes()["app_id"], assertions.ShouldResemble, attributes["app_id"])
+	a.So(got.Attributes()["count"], assertions.ShouldAlmostEqual, attributes["count"])
+}
+
+func TestFromUnspecifiedGRPC(t *testing.T) {
+	a := assertions.New(t)
+
+	err := grpc.Errorf(codes.DeadlineExceeded, "This is an error")
+
+	got := FromGRPC(err)
+	a.So(got.Code(), assertions.ShouldEqual, 0)
+	a.So(got.Type(), assertions.ShouldEqual, Timeout)
+	a.So(got.Error(), assertions.ShouldEqual, "This is an error")
+	a.So(got.Attributes(), assertions.ShouldBeNil)
+}
