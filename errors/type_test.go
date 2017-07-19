45bf5006a64d309f15eba5e9ef81d8eb729bec5e
Romeo Van Snick <romeovs@gmail.com>
2017-07-19 17:19:03 +0200

    Test round trip of error types

--- errors/type_test.go
+++ errors/type_test.go
@@ -4,35 +4,60 @@
 package errors
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/smartystreets/assertions"
 )
 
+var types = map[string]Type{
+	"Unknown":                 Unknown,
+	"Internal":                Internal,
+	"Invalid argument":        InvalidArgument,
+	"Out of range":            OutOfRange,
+	"Not found":               NotFound,
+	"Conflict":                Conflict,
+	"Already exists":          AlreadyExists,
+	"Unauthorized":            Unauthorized,
+	"Permission denied":       PermissionDenied,
+	"Timeout":                 Timeout,
+	"Not implemented":         NotImplemented,
+	"Temporarily unavailable": TemporarilyUnavailable,
+	"Permanently unavailable": PermanentlyUnavailable,
+	"Canceled":                Canceled,
+	"Resource exhausted":      ResourceExhausted,
+}
+
 func TestTypeString(t *testing.T) {
 	a := assertions.New(t)
 
-	a.So(Unknown.String(), assertions.ShouldEqual, "Unknown")
-	a.So(Timeout.String(), assertions.ShouldEqual, "Timeout")
+	for str, typ := range types {
+		a.So(typ.String(), assertions.ShouldEqual, str)
+	}
 }
 
 func TestTypeMarshal(t *testing.T) {
 	a := assertions.New(t)
 
-	text, err := Unknown.MarshalText()
-	a.So(err, assertions.ShouldBeNil)
-	a.So(text, assertions.ShouldResemble, []byte("Unknown"))
+	for str, typ := range types {
+		text, err := typ.MarshalText()
+		a.So(err, assertions.ShouldBeNil)
+		a.So(text, assertions.ShouldResemble, []byte(str))
+	}
 }
 
 func TestTypeUnmarshal(t *testing.T) {
 	a := assertions.New(t)
 
-	var typ Type
-	err := typ.UnmarshalText([]byte("Temporarily unavailable"))
-	a.So(err, assertions.ShouldBeNil)
-	a.So(typ, assertions.ShouldEqual, TemporarilyUnavailable)
+	for str, typ := range types {
+		var res Type
+		err := res.UnmarshalText([]byte(str))
+		a.So(err, assertions.ShouldBeNil)
+		a.So(res, assertions.ShouldEqual, typ)
 
-	err = typ.UnmarshalText([]byte("temporarily unavailable"))
-	a.So(err, assertions.ShouldBeNil)
-	a.So(typ, assertions.ShouldEqual, TemporarilyUnavailable)
+		err = res.UnmarshalText([]byte(strings.ToLower(str)))
+		a.So(err, assertions.ShouldBeNil)
+		a.So(res, assertions.ShouldEqual, typ)
+	}
+
 }
